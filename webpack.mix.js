8fd2cbcad0dfcd7942444fd1b4d0b1f17fd16c66
Mohammad Salehi <mswinuser@yahoo.com>
2021-05-05 21:37:37 +0430

    Added sass structure files.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,9 @@
 let mix = require('laravel-mix');
 const js_input = "root-styler/assets/src/js/app.js";
 const js_output = "root-styler/assets/js/app.js";
-mix.babel(js_input,js_output);
+const sass_in = "root-styler/assets/src/sass/app.scss";
+const sass_out = "root-styler/assets/css/app.css";
+mix.babel(js_input,js_output)
+    .sass(sass_in,sass_out).options({
+    processCssUrls: false
+});
